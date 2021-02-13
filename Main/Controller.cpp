81da56a74ffcb54a16b2d50509d1eb94560a0f57
Russell Grim <russellgrim@gmail.com>
2021-02-13 15:57:32 +0000

    inital files

---
+++ Main/Controller.cpp
@@ -0,0 +1,14 @@
+#include "Controller.h"
+
+
+Controller::Controller () {;} // Constructor
+
+Controller::~Controller() {;} // Destructor
+
+void Controller::setup() {
+    ;
+}
+
+void Controller::loop() {
+    ;
+}
---
+++ Main/Controller.h
@@ -0,0 +1,21 @@
+#ifndef Controller_h
+#define Controller_h
+
+#include "Arduino.h"
+
+class Controller {
+public:
+    Controller (); //constructor
+    ~Controller(); // Destructor
+    void setup();
+    void loop();
+
+private:
+
+};
+
+
+
+
+
+#endif
